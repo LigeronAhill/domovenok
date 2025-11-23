19da5d45e4848df29cc537e6e1d00b13623ddaf2
Aleksandr Provotorov <provotorov@inbox.ru>
2025-11-23 12:00:55 +0300

    init logger

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"os"
 	"strings"
 
 	"github.com/LigeronAhill/domovenok/internal/config"
+	"github.com/LigeronAhill/domovenok/internal/logger"
 )
 
 func main() {
 	env := "local"
+	level := logger.DEBUG
 	if prod := os.Getenv("ENVIRONMENT"); prod != "" {
 		if strings.ToLower(prod) == "production" {
 			env = "production"
+			level = logger.INFO
 		}
 	}
-	_, err := config.Init(env)
+	logger.Init(level)
+	settings, err := config.Init(env)
 	if err != nil {
 		log.Fatal(err)
 	}
+	if settings == nil {
+		log.Fatal("не удалось загрузить конфигурацию")
+	}
+	slog.Info("Конфигурация успешно загружена", slog.String("Окружение", env))
 }
